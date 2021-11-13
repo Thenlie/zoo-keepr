c7e40065846e65f439a074c398b2087bd2a0ba62
Leithen <leithen113@gmail.com>
2021-11-13 11:47:01 -0700

    Added PORT variable

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const { animals } = require ('./data/animals.json');
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -38,6 +38,6 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log('API server now on port 3001!');
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
